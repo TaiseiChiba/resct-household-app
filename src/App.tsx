1f0afd36d28584be1851b57e2c54d2f42e6cae87
Taisei Chiba <nanakorobiyaoki.tai@icloud.com>
2024-09-23 19:24:45 +0900

    レイアウトファイルの作成

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Report from './pages/Report';
+import NoMatch from './pages/NoMatch';
+import AppLayout from './components/AppLayout';
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<AppLayout />} >
+          <Route index element={<Home />} />
           <Route path="/report" element={<Report />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
       </Routes>
     </Router>
   );
