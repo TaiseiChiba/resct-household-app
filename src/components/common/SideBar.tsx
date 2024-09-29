e63bd0a83e78aa84e4c5ed3af94b5e59190aa614
Taisei Chiba <nanakorobiyaoki.tai@icloud.com>
2024-09-29 17:37:52 +0900

    サイドバーのスタイルを修正

--- src/components/common/SideBar.tsx
+++ src/components/common/SideBar.tsx
@@ -5,16 +5,17 @@ import HomeIcon from '@mui/icons-material/Home';
 import MailIcon from '@mui/icons-material/Mail';
 import AssessmentIcon from '@mui/icons-material/Assessment';
 
-import React from 'react'
+import React, { CSSProperties } from 'react'
+import { NavLink } from 'react-router-dom';
 
 /**
  * サイドバープロップス
  * サイドバーの型をまとめたインターフェイス
  */
 interface SideBarProps {
-  drawerWidth:number, 
-  mobileOpen: boolean, 
-  handleDrawerTransitionEnd: () => void, 
+  drawerWidth: number,
+  mobileOpen: boolean,
+  handleDrawerTransitionEnd: () => void,
   handleDrawerClose: () => void
 }
 
@@ -24,12 +25,22 @@ interface SideBarItem {
   icon: React.ComponentType
 }
 
-const SideBar = ({drawerWidth, mobileOpen, handleDrawerTransitionEnd, handleDrawerClose}: SideBarProps) => {
+const SideBar = ({ drawerWidth, mobileOpen, handleDrawerTransitionEnd, handleDrawerClose }: SideBarProps) => {
 
   const sideBarItems: SideBarItem[] = [
-    {text: "Home", path: "/", icon: HomeIcon},
-    {text: "Report", path: "/report", icon: AssessmentIcon}
+    { text: "Home", path: "/", icon: HomeIcon },
+    { text: "Report", path: "/report", icon: AssessmentIcon }
   ]
+
+  const baseLinkStyle: CSSProperties = {
+    textDecoration: "none",
+    color: "inherit",
+    display: "block"
+  }
+
+  const activeLinkStyle: CSSProperties = {
+    backgroundColor: "rgba(0, 0, 0, 0.08)"
+  }
 
   const drawer = (
     <div>
@@ -37,15 +48,22 @@ const SideBar = ({drawerWidth, mobileOpen, handleDrawerTransitionEnd, handleDraw
       <Divider />
       <List>
         {sideBarItems.map((item, index) => (
-          <ListItem key={index} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
-                < item.icon />
-              </ListItemIcon>
-              <ListItemText primary={item.text} />
-            </ListItemButton>
-          </ListItem>
+          <NavLink key={index} to={item.path} style={({isActive}) => {
+            return {
+              ...baseLinkStyle,
+              ...(isActive ? activeLinkStyle : {})
+            }
+          }}>
+            <ListItem key={index} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {/* {index % 2 === 0 ? <InboxIcon /> : <MailIcon />} */}
+                  < item.icon />
+                </ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItemButton>
+            </ListItem>
+          </NavLink>
         ))}
       </List>
     </div>
@@ -53,40 +71,40 @@ const SideBar = ({drawerWidth, mobileOpen, handleDrawerTransitionEnd, handleDraw
 
   return (
     <Box
-        component="nav"
-        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
-        aria-label="mailbox folders"
+      component="nav"
+      sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+      aria-label="mailbox folders"
+    >
+      { /* モバイル用 */}
+      <Drawer
+        variant="temporary"
+        open={mobileOpen}
+        onTransitionEnd={handleDrawerTransitionEnd}
+        onClose={handleDrawerClose}
+        ModalProps={{
+          keepMounted: true, // Better open performance on mobile.
+        }}
+        // xs=0ピクセル以上なら、blockとして表示 
+        // xs=600ピクセル以上なら、noneとして表示 
+        sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
       >
-        { /* モバイル用 */}
-        <Drawer
-          variant="temporary"
-          open={mobileOpen}
-          onTransitionEnd={handleDrawerTransitionEnd}
-          onClose={handleDrawerClose}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
-          // xs=0ピクセル以上なら、blockとして表示 
-          // xs=600ピクセル以上なら、noneとして表示 
-          sx={{
-            display: { xs: 'block', sm: 'none' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-        >
-          {drawer}
-        </Drawer>
-        { /* PC用 */}
-        <Drawer
-          variant="permanent"
-          sx={{
-            display: { xs: 'none', sm: 'block' },
-            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
-          }}
-          open
-        >
-          {drawer}
-        </Drawer>
-      </Box>
+        {drawer}
+      </Drawer>
+      { /* PC用 */}
+      <Drawer
+        variant="permanent"
+        sx={{
+          display: { xs: 'none', sm: 'block' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+        }}
+        open
+      >
+        {drawer}
+      </Drawer>
+    </Box>
   )
 }
 
